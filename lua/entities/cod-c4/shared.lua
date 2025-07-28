c6e023debf131d4d7ddf83e7e1360f01ec258bdc
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2025-07-28 12:43:43 -0400

    Property usage restrictions

--- lua/entities/cod-c4/shared.lua
+++ lua/entities/cod-c4/shared.lua
@@ -6,57 +6,61 @@ ENT.Author			= "Hoff"
 ENT.Spawnable			= false
 ENT.AdminSpawnable		= false
 
+function ENT:CanProperty(_, property)
+	if property == "collision" then return false end
+end
+
 if !ConVarExists("C4_DoorSearchRadius") then
-    CreateConVar("C4_Infinite", 0, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "Should C4 be infinite? 1 = infinite", 0, 1)
-    CreateConVar("C4_ThrowSpeed", 1, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "How long is the delay between C4 throws?", 0.1, 10)
-    CreateConVar("C4_Magnitude", 175, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "How strong is the C4 explosion?", 1, 500)
-    CreateConVar("C4_KnockDoors", 0, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "Should C4 knock down doors?", 0, 1)
-    CreateConVar("C4_DoorKnockStrength", 500, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "How hard should the door be blasted?", 100, 2500)
-    CreateConVar("C4_DoorSearchRadius", 75, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "How far away should doors be effected?", 1, 500)
+	CreateConVar("C4_Infinite", 0, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "Should C4 be infinite? 1 = infinite", 0, 1)
+	CreateConVar("C4_ThrowSpeed", 1, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "How long is the delay between C4 throws?", 0.1, 10)
+	CreateConVar("C4_Magnitude", 175, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "How strong is the C4 explosion?", 1, 500)
+	CreateConVar("C4_KnockDoors", 0, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "Should C4 knock down doors?", 0, 1)
+	CreateConVar("C4_DoorKnockStrength", 500, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "How hard should the door be blasted?", 100, 2500)
+	CreateConVar("C4_DoorSearchRadius", 75, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "How far away should doors be effected?", 1, 500)
 end
 
 if CLIENT then
-    if !ConVarExists("C4_RedLight") then
-        CreateClientConVar("C4_RedLight", 1, true)
-    end
-    local function funcCallback(CVar, _, NewValue)
-        net.Start("C4_Convars_Change", true)
-        net.WriteString(CVar)
-        net.WriteFloat(tonumber(NewValue))
-        net.SendToServer()
-    end
-    cvars.AddChangeCallback("C4_Infinite", funcCallback)
-    cvars.AddChangeCallback("C4_ThrowSpeed", funcCallback)
-    cvars.AddChangeCallback("C4_Magnitude", funcCallback)
-    cvars.AddChangeCallback("C4_KnockDoors", funcCallback)
-    cvars.AddChangeCallback("C4_DoorKnockStrength", funcCallback)
-    cvars.AddChangeCallback("C4_DoorSearchRadius", funcCallback)
+	if !ConVarExists("C4_RedLight") then
+		CreateClientConVar("C4_RedLight", 1, true)
+	end
+	local function funcCallback(CVar, _, NewValue)
+		net.Start("C4_Convars_Change", true)
+		net.WriteString(CVar)
+		net.WriteFloat(tonumber(NewValue))
+		net.SendToServer()
+	end
+	cvars.AddChangeCallback("C4_Infinite", funcCallback)
+	cvars.AddChangeCallback("C4_ThrowSpeed", funcCallback)
+	cvars.AddChangeCallback("C4_Magnitude", funcCallback)
+	cvars.AddChangeCallback("C4_KnockDoors", funcCallback)
+	cvars.AddChangeCallback("C4_DoorKnockStrength", funcCallback)
+	cvars.AddChangeCallback("C4_DoorSearchRadius", funcCallback)
 end
 
 if SERVER then
-    util.AddNetworkString("C4_Convars_Change")
+	util.AddNetworkString("C4_Convars_Change")
 
-    net.Receive("C4_Convars_Change", function(_, ply)
-        if !ply:IsAdmin() then return end
-        local cvar_name = net.ReadString()
-        local cvar_val = net.ReadFloat()
-        RunConsoleCommand(cvar_name, cvar_val)
-    end)
+	net.Receive("C4_Convars_Change", function(_, ply)
+		if !ply:IsAdmin() then return end
+		local cvar_name = net.ReadString()
+		local cvar_val = net.ReadFloat()
+		RunConsoleCommand(cvar_name, cvar_val)
+	end)
 elseif CLIENT then
-    hook.Add("PopulateToolMenu", "AddC4SettingsPanel", function()
-        spawnmenu.AddToolMenuOption("Utilities", "Hoff's Addons", "C4SettingsPanel", "C4 Setup", "", "", function(cpanel)
-            if !game.SinglePlayer() and !LocalPlayer():IsAdmin() then
-                cpanel:CheckBox("C4 Red Light", "C4_RedLight")
-                return
-            end
+	hook.Add("PopulateToolMenu", "AddC4SettingsPanel", function()
+		spawnmenu.AddToolMenuOption("Utilities", "Hoff's Addons", "C4SettingsPanel", "C4 Setup", "", "", function(cpanel)
+			if !game.SinglePlayer() and !LocalPlayer():IsAdmin() then
+				cpanel:CheckBox("C4 Red Light", "C4_RedLight")
+				return
+			end
 
-            cpanel:CheckBox("Infinite C4", "C4_Infinite")
-            cpanel:NumSlider("C4 Magnitude", "C4_Magnitude", 1, 500, 0)
-            cpanel:NumSlider("C4 Throw Speed", "C4_ThrowSpeed", 0.1, 10, 2)
-            cpanel:CheckBox("Knock Down Doors", "C4_KnockDoors")
-            cpanel:NumSlider("Door Knock Strength", "C4_DoorKnockStrength", 100, 2500, 0)
-            cpanel:NumSlider("Door Search Radius", "C4_DoorSearchRadius", 1, 500, 0)
-            cpanel:CheckBox("C4 Red Light", "C4_RedLight")
-        end)
-    end)
+			cpanel:CheckBox("Infinite C4", "C4_Infinite")
+			cpanel:NumSlider("C4 Magnitude", "C4_Magnitude", 1, 500, 0)
+			cpanel:NumSlider("C4 Throw Speed", "C4_ThrowSpeed", 0.1, 10, 2)
+			cpanel:CheckBox("Knock Down Doors", "C4_KnockDoors")
+			cpanel:NumSlider("Door Knock Strength", "C4_DoorKnockStrength", 100, 2500, 0)
+			cpanel:NumSlider("Door Search Radius", "C4_DoorSearchRadius", 1, 500, 0)
+			cpanel:CheckBox("C4 Red Light", "C4_RedLight")
+		end)
+	end)
 end
